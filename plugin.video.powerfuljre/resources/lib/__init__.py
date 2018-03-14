5f52aee7a8d7f85c13146e7a990e1c58e104c33e
muaddibttv <31803191+muaddibttv@users.noreply.github.com>
2018-03-14 17:19:15 -0400

    JRE Addon

---
+++ plugin.video.powerfuljre/resources/lib/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#######################################################################
+ # ----------------------------------------------------------------------------
+ # "THE BEER-WARE LICENSE" (Revision 42):
+ # @tantrumdev wrote this file.  As long as you retain this notice you
+ # can do whatever you want with this stuff. If we meet some day, and you think
+ # this stuff is worth it, you can buy me a beer in return. - Muad'Dib
+ # ----------------------------------------------------------------------------
+#######################################################################
+
+# Addon Name: The Joe Rogan Experience
+# Addon id: plugin.video.powerfuljre
+# Addon Provider: MuadDib
